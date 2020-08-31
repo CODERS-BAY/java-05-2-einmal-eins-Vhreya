d6a2a89024aa12d199c7e723cd72c76d12229b63
Vanessa <va.au@gmx.at>
2020-08-31 10:05:40 +0200

    rdy for review

---
+++ src/Multiplications.java
@@ -0,0 +1,11 @@
+public class Multiplications {
+    public static void main(String[] args) {
+
+        for (int a = 1; a <= 10; a++) {
+            for (int b = 1; b <= 10; b++) {
+                System.out.printf("%d * %d = %d%n", a, b, a * b);
+            }
+            System.out.println();
+        }
+    }
+}
